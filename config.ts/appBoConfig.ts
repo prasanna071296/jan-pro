ef3e951dd45b5f2a66ac9bcc514e028c11ae0d0e
prasanna071296 <prasannajancy1996@gmail.com>
2025-10-31 18:02:50 +0530

    hard code data

---
+++ config.ts/appBoConfig.ts
@@ -0,0 +1,11 @@
+export const appBoConfig={
+    baseUrl: 'https://dev2.exitsmarts.com',
+    businessOwner :{
+        email:'prasannajan07+regbo@gmail.com',
+        password:'Secret@1234567',
+    },
+    timeouts:{
+        default:10000,
+        long:30000,
+    },
+}
---
+++ config.ts/appConfig.ts
@@ -0,0 +1,11 @@
+export const appConfig = {
+  baseUrl: 'https://dev2.exitsmarts.com',
+  advisor: {
+    email: 'prasannajan07+regdev@gmail.com',
+    password: 'Secret@1234567',
+  },
+  timeouts: {
+    default: 10000,
+    long: 30000,
+  },
+};
